3c01b57d70d9c7dda51d66524142db4bebf866ec
ZhiquAI <ziyouzhe1984@gmail.com>
2025-07-01 07:37:22 +0800

    Fix PDF.js Worker Loading Error

--- src/components/common/PDFViewer.tsx
+++ src/components/common/PDFViewer.tsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Button, Spin, Alert, Progress, Tag } from 'antd';
 import { ZoomInOutlined, ZoomOutOutlined, EyeOutlined, LeftOutlined, RightOutlined, DeleteOutlined } from '@ant-design/icons';
 import * as pdfjsLib from 'pdfjs-dist';
+import pdfWorker from 'pdfjs-dist/build/pdf.worker.min.js?url';
 
 // 设置PDF.js worker - 使用Vite兼容的方式
-pdfjsLib.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.js', import.meta.url).href;
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorker;
 
 interface PDFViewerProps {
   file: File;
