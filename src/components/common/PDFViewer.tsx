dd003fb85d87f3c20bc5c3c685f0c4b8cbb13b03
ZhiquAI <ziyouzhe1984@gmail.com>
2025-07-01 12:43:36 +0800

    Fix PDF Worker and Question Recognition Errors

--- src/components/common/PDFViewer.tsx
+++ src/components/common/PDFViewer.tsx
@@ -4,10 +4,8 @@ import { ZoomInOutlined, ZoomOutOutlined, EyeOutlined, LeftOutlined, RightOutlin
 import * as pdfjsLib from 'pdfjs-dist';
 
 // 设置PDF.js worker - 使用Vite兼容的方式
-pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url
-).toString();
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 interface PDFViewerProps {
   file: File;
